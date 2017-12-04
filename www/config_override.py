d3ec73e9d46348ff2b3781dfc9eab1bb8d35466e
Spike <xytx1989@vip.qq.com>
2017-12-04 22:55:30 +0800

    复制所有工程

---
+++ www/config_override.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Override configurations.
+'''
+
+
+
+configs = {
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'root',
+        'password': 'HYL19900203lp',
+        'db': 'awesome'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
